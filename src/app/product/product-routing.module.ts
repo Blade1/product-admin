bba0474709095362df34ef78af97ce3df56bb06c
Blade1 <blade.test.craft@gmail.com>
2020-09-10 17:36:33 +0300

    1 file modified

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-import { ProductListComponent } from "./product/product-list/pfoduct-list.component";
-import { ProductFormComponent } from "./product/product-form/pfoduct-form.component";
-import { SelectProductMessageComponent } from './product/select-product-message/select-product-message.component';
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { ProductListComponent } from "./product-list/pfoduct-list.component";
+import { ProductFormComponent } from "./product-form/pfoduct-form.component";
+import { SelectProductMessageComponent } from './select-product-message/select-product-message.component';
+import { PageNotFoundComponent } from "../page-not-found/page-not-found.component";
 
 const crisisCenterRoutes: Routes = [
   {
