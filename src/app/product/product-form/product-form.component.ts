c2b797257f26a755b6af6b4709bcf43be2c32d37
Yura <mytsko.yura@gmail.com>
2020-09-10 18:38:57 +0300

    added product to product form

--- src/app/product/product-form/product-form.component.ts
+++ src/app/product/product-form/product-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Product } from '../product';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-form',
@@ -6,10 +11,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
+  product: Observable<Product>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: ProductService
+  ) {}
 
   ngOnInit() {
+    this.product = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.service.getProduct(params.get('id')))
+    );
   }
 
 }
--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {PRODUCTS} from './products-mock';
 import { Observable, of } from 'rxjs';
 import { Product } from "./product";
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
@@ -11,5 +12,13 @@ export class ProductService {
   getProducts(): Observable<Product[]> {
     console.log("Got products");
     return of(PRODUCTS);
+  }
+
+  getProduct(id: string) {
+    console.log("Got product with id " + id);
+    return this.getProducts().pipe(
+      // (+) before `id` turns the string into a number
+      map((heroes: Product[]) => heroes.find(product => product.id === id))
+    );
   }
 }
