c88cce086421a9314e5bfe4c94d0fe0ec7ef6496
Yura <mytsko.yura@gmail.com>
2020-09-10 18:10:02 +0300

    update 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-// import { ProductListComponent } from "./product/product-list/pfoduct-list.component";
-import { ProductModule } from './product/product.module';
+import { ProductListComponent } from "./product/product-list/product-list.component";
+import { ProductFormComponent } from "./product/product-form/product-form.component";
+import { SelectProductMessageComponent } from './product/select-product-message/select-product-message.component';
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { CategoryListComponent } from "./category/category-list/category-list.component";
 
 const appRoutes: Routes = [
+    {
+    path: "admin/products",
+    component: ProductListComponent,
+    children: [
+      {
+        path: ":id",
+        component: ProductFormComponent
+      },
+      {
+        path: "",
+        component: SelectProductMessageComponent
+      }
+    ]
+  },
   { path: "admin/categories", component: CategoryListComponent },
-  { path: "admin/products", component: ProductModule},
   { path: "", redirectTo: "/admin/categories", pathMatch: "full" },
   { path: "**", component: PageNotFoundComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductService } from './product/product.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CategoryListComponent } from './category/category-list/category-list.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { Router } from '@angular/router';
-import { ProductFormComponent } from './product/product-form/product-form.component';
+
+import { ProductListComponent } from "./product/product-list/product-list.component";
+import { ProductFormComponent } from "./product/product-form/product-form.component";
 import { SelectProductMessageComponent } from './product/select-product-message/select-product-message.component';
-import { ProductModule } from './product/product.module';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [ AppComponent, ProductListComponent, PageNotFoundComponent, CategoryListComponent, ProductFormComponent, SelectProductMessageComponent, ProductModule ],
+  declarations: [ AppComponent, PageNotFoundComponent, CategoryListComponent, ProductListComponent, ProductFormComponent, SelectProductMessageComponent],
   bootstrap:    [ AppComponent ],
   providers: [ProductService]
 })
